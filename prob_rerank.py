16ba6cf8241477307c289e7518a2a94fa108f866
Subhalingam D <subhalingam.d@gmail.com>
2020-11-14 19:20:15 +0530

    IMPORTANT BUG FIX!!!!

--- prob_rerank.py
+++ prob_rerank.py
@@ -67,7 +67,7 @@ def do_task(docid_file_offset,qtext,result_docs,collection_file,expansion_limit,
 	# r = the number of known relevant documents term t(i) occurs in
 	# n = the number of documents term t(i) occurs in = df(i)
 
-	for word,r in df_rel_doc_set.values():
+	for word,r in df_rel_doc_set.items():
 		n = vocab_words_df[word]
 		score = r * log ( ( (r+0.5)*(N-n-R+r+0.5) ) / ( (n-r+0.5)*(R-r+0.5) ) )
 		# just update the values
